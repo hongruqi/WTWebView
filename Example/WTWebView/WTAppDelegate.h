597606347ef503bce755d132e6ebefd81a22b472
hongru qi <hongru.qi@quvideo.com>
2017-08-11 09:49:53 +0800

    Initial commit

---
+++ Example/WTWebView/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  WTWebView
+//
+//  Created by lbrsilva-allin on 08/11/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WTWebView/WTViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTViewController.h
+//  WTWebView
+//
+//  Created by lbrsilva-allin on 08/11/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTViewController : UIViewController
+
+@end
